661018953ff59b5385a30eedcefd29a9588fb3e0
JoaquimRS <joaquimdaweb@gmail.com>
2022-12-12 20:04:06 +0100

    WorkFlow correcte

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let express = require("express")
 let app = express()
 let port = 3000
 
-app.get('/', (req, res) => res.send('Hola que tal, soc Joaquim'))
+app.get('/', (req, res) => res.send("Hola que tal, soc Joaquim"))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log("Example app listening at http://localhost:${port}"))
